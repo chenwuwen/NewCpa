64f19be5012ef93b826cb5556869ee155a9bb5a1
kanyun <949955482@qq.com>
2017-04-20 14:27:42 +0800

    改变原有load加载页面改用angularJs以及angular-ui-router来实现页面切换

--- src/main/webapp/scripts/index.js
+++ src/main/webapp/scripts/index.js
@@ -15,11 +15,11 @@
 
 //定义自己的module(routerApp)
 //中括号中的是这个module的依赖
-var routerApp = angular.module('myapp', ['ui.router']);
-routerApp.config(function ($stateProvider, $urlRouterProvider) {
+var app = angular.module('myapp', ['ui.router']);
+app.config(function ($stateProvider, $urlRouterProvider) {
     $stateProvider
         .state('navbar', {  //路由状态
-            url: '/navbar/:typeCode',  //路由路径
+            url: '/:typeCode',  //路由路径
             templateUrl: 'navbar.html',  //路由填充的模板
             controller: 'navbarCtrl' //此处若写controller,则必须指定一个存在的组件,否则页面可能不正常显示
         })
@@ -27,6 +27,7 @@ routerApp.config(function ($stateProvider, $urlRouterProvider) {
             url: '/main',  //路由路径
             templateUrl: 'main.html',//路由填充的模板
             controller: 'mainCtrl'
+            // params:{} //目标页面定义接收参数
         })
         .state('navbar.unitExam', {  //路由状态
             url: '/unitExam', //路由路径
@@ -37,12 +38,29 @@ routerApp.config(function ($stateProvider, $urlRouterProvider) {
     $urlRouterProvider.otherwise('/navbar/main');
 });
 // 定义navbarCtrl控制器，对应路由中设置的controller
-routerApp.controller('navbarCtrl', function ($scope, $stateParams) {
-    console.log($stateParams.typeCode);
+app.controller('navbarCtrl', function ($scope, $stateParams, $http) {
+    // console.log($stateParams.typeCode);
+    // $scope.send = function(){
+    //     $http({
+    //         method: 'POST',
+    //         url: '/cpa/unitExam/getUnitExam.do',
+    //         params:{'typeCode':'1'}
+    //     }).then(function successCallback(response) {
+    //         // 请求成功执行代码
+    //         alert('success');
+    //     }, function errorCallback(response) {
+    //         // 请求失败执行代码
+    //         alert('error');
+    //     })
+    //
+    // }
     }
 );
-routerApp.controller('mainCtrl',function($scope){
+app.controller('mainCtrl',function($scope){
     $scope.name="kanyun"
-})
+});
+app.controller('UnitExamCtrl',function($scope){
+    // $scope.name="kanyun"
+});
 
 

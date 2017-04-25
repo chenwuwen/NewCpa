bff203d779c256ea5318d2a0fb396d4b74f17fd7
kanyun <949955482@qq.com>
2017-04-25 09:59:03 +0800

    aaa

--- src/main/webapp/scripts/UnitExamCtrl.js
+++ src/main/webapp/scripts/UnitExamCtrl.js
@@ -18,3 +18,25 @@ app.controller('UnitExamCtrl',function($scope, $stateParams, $http){
         alert('error');
     })
 });
+app.filter('ABCDEFGH',function () { //可以注入依赖
+    return function (x) {
+        switch (x){
+            case 0:
+                return 'A';
+            case 1:
+                return 'B';
+            case 2:
+                return 'C';
+            case 3:
+                return 'D';
+            case 4:
+                return 'E';
+            case 5:
+                return 'F';
+            case 6:
+                return 'G';
+            case 7:
+                return 'H';
+        }
+    }
+});
